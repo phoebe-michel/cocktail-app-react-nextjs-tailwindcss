06576bb6d57d4d90b4cfc9646d5b72d0788632d7
Phoebe Michel <awkwardblackcoder@gmail.com>
2024-05-23 16:15:44 -0400

    update nav items

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -4,23 +4,19 @@ const Nav = () => {
   return (
     <nav className="fixed top-0 z-10 w-screen text-white bg-[#333]/[0.2] shadow-xl">
       <div className="container mx-auto ">
-        <div className="flex justify-between text-2xl">
-          <div className="logo py-5 text-4xl font-semibold text-white cursor-pointer">
+        <div className="flex justify-between items-center text-base">
+          <div className="logo py-5 text-2xl font-semibold text-white cursor-pointer">
             C&C
           </div>
           <ul className="flex">
-            <li className="px-8 py-5 hover:text-[#ff0033] cursor-pointer">
-              Home
-            </li>
-            <li className="px-8 py-5 hover:text-[#ff0033] cursor-pointer">
+            <li className="p-5 hover:text-[#ff0033] cursor-pointer">Home</li>
+            <li className="p-5 hover:text-[#ff0033] cursor-pointer">
               Categories
             </li>
-            <li className="px-8 py-5 hover:text-[#ff0033] cursor-pointer">
+            <li className="p-5 hover:text-[#ff0033] cursor-pointer">
               My Recipes
             </li>
-            <li className="px-8 py-5 hover:text-[#ff0033] cursor-pointer">
-              Login
-            </li>
+            <li className="p-5 hover:text-[#ff0033] cursor-pointer">Login</li>
           </ul>
         </div>
       </div>
